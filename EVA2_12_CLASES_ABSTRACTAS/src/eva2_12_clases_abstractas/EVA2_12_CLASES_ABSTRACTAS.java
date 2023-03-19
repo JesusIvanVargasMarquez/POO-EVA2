016d226e0e2103ae384d089971d6dc452347d668
JesusIvanVargasMarquez <124702508+JesusIvanVargasMarquez@users.noreply.github.com>
2023-03-19 14:10:13 -0600

    Add files via upload

---
+++ EVA2_12_CLASES_ABSTRACTAS/src/eva2_12_clases_abstractas/EVA2_12_CLASES_ABSTRACTAS.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eva2_12_clases_abstractas;
+
+/**
+ *
+ * @author ivan2
+ */
+public class EVA2_12_CLASES_ABSTRACTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //no se pueden crear objetos de clases abstractas
+        pruebaAbstracta pabs = new PruebaAbstracta();
+    }
+    
+}
+
+class prueba{
+    
+}
+
+abstract class pruebaAbstracta{
+    
+}
